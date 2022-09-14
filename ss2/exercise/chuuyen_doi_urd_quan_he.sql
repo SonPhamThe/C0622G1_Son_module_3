a5855e44c02ac5c436363d13e4d2e668fb4de682
Son Pham <thesonpham120499@gmail.com>
2022-09-14 09:00:21 +0700

    new task3

--- ss2/exercise/chuuyen_doi_urd_quan_he.sql
+++ ss2/exercise/chuuyen_doi_urd_quan_he.sql
@@ -1,57 +1,57 @@
 DROP DATABASE IF EXISTS chuyen_doi_erd_mo_hinh_quan_he;
 CREATE DATABASE chuyen_doi_erd_mo_hinh_quan_he;
 USE chuyen_doi_erd_mo_hinh_quan_he;
-CREATE TABLE nha_CC(
-ma_NCC INT AUTO_INCREMENT PRIMARY KEY,
-ten_NCC VARCHAR(45),
+CREATE TABLE nha_cc(
+ma_ncc INT AUTO_INCREMENT PRIMARY KEY,
+ten_ncc VARCHAR(45),
 dia_chi VARCHAR(45)
 );
 CREATE TABLE chi_tiet_SDT(
 so_dien_thoai VARCHAR(20),
 ma_dien_thoai VARCHAR(20),
-ma_NCC INT,
-FOREIGN KEY(ma_NCC) REFERENCES nha_CC(ma_NCC)
+ma_ncc INT,
+FOREIGN KEY(ma_ncc) REFERENCES nha_cc(ma_ncc)
 );
 CREATE TABLE vat_tu(
-ma_VT INT AUTO_INCREMENT PRIMARY KEY,
-ten_VT VARCHAR(20)
+ma_vt INT AUTO_INCREMENT PRIMARY KEY,
+ten_vt VARCHAR(20)
 );
-CREATE TABLE don_DH(
-so_DH INT AUTO_INCREMENT PRIMARY KEY,
-ngay_DH DATE,
-ma_NCC INT,
-FOREIGN KEY(ma_NCC) REFERENCES nha_CC(ma_NCC)
+CREATE TABLE don_dh(
+so_dh INT AUTO_INCREMENT PRIMARY KEY,
+ngay_dh DATE,
+ma_ncc INT,
+FOREIGN KEY(ma_ncc) REFERENCES nha_cc(ma_ncc)
 );
-CREATE TABLE chi_tiet_don_DH(
-ma_VT INT,
-so_DH INT,
-PRIMARY KEY(ma_VT,so_DH),
-FOREIGN KEY(ma_VT) REFERENCES vat_tu(ma_VT),
-FOREIGN KEY(so_DH) REFERENCES don_DH(so_DH)
+CREATE TABLE chi_tiet_don_dh(
+ma_vt INT,
+so_dh INT,
+PRIMARY KEY(ma_vt,so_dh),
+FOREIGN KEY(ma_vt) REFERENCES vat_tu(ma_vt),
+FOREIGN KEY(so_dh) REFERENCES don_dh(so_dh)
 );
 CREATE TABLE phieu_nhap(
-so_PN INT AUTO_INCREMENT PRIMARY KEY,
+so_pn INT AUTO_INCREMENT PRIMARY KEY,
 ngay_nhap DATE
 );
 CREATE TABLE chi_tiet_phieu_nhap(
-DG_nhap DOUBLE,
-SL_nhap DOUBLE,
-ma_VT INT,
-so_PN INT,
-PRIMARY KEY(ma_VT,so_PN),
-FOREIGN KEY(ma_VT) REFERENCES vat_tu(ma_VT),
-FOREIGN KEY(so_PN) REFERENCES phieu_nhap(so_PN)
+dg_nhap DOUBLE,
+sl_nhap DOUBLE,
+ma_vt INT,
+so_pn INT,
+PRIMARY KEY(ma_vt,so_pn),
+FOREIGN KEY(ma_vt) REFERENCES vat_tu(ma_vt),
+FOREIGN KEY(so_pn) REFERENCES phieu_nhap(so_pn)
 );
 CREATE TABLE phieu_xuat(
-so_PX INT AUTO_INCREMENT PRIMARY KEY,
+so_px INT AUTO_INCREMENT PRIMARY KEY,
 ngay_xuat DATE
 );
 CREATE TABLE chi_tiet_phieu_xuat(
-DG_xuat DOUBLE,
-SL_xuat DOUBLE,
-so_PX INT,
-ma_VT INT,
-PRIMARY KEY(so_PX,ma_VT),
-FOREIGN KEY(so_PX) REFERENCES phieu_xuat(so_PX),
-FOREIGN KEY(ma_VT) REFERENCES vat_tu(ma_VT)
+dg_xuat DOUBLE,
+sl_xuat DOUBLE,
+so_px INT,
+ma_vt INT,
+PRIMARY KEY(so_px,ma_vt),
+FOREIGN KEY(so_px) REFERENCES phieu_xuat(so_px),
+FOREIGN KEY(ma_vt) REFERENCES vat_tu(ma_vt)
 );
